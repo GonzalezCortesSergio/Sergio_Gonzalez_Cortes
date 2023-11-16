b33c173239dd2892f68b9dd9b9ef07519f7acebd
Sergio González Cortés <gonzalez.coser23@triana.salesianos.edu>
2023-11-16 21:35:52 +0100

    Ejercicio 2 boletin 1 BD terminado

--- Base de Datos/UD4/Boletín de ejercicio 1/Ejercicio 1/Ejercicio 2.sql
+++ Base de Datos/UD4/Boletín de ejercicio 1/Ejercicio 1/Ejercicio 2.sql
@@ -19,3 +19,57 @@ FROM employees j
 	JOIN employees e ON (e.manager_id = j.employee_id)
 	JOIN departments d ON (d.department_id = e.department_id)
 WHERE d.department_name = 'Administration';
+
+
+--3: Seleccionar el COUNTRY_NAME donde tiene localización el departamento 
+--de Public Relations
+
+SELECT country_name
+FROM countries c
+	JOIN locations l USING (country_id)
+	JOIN departments d USING (location_id)
+WHERE department_name = 'Public Relations';
+
+--4: Seleccionar aquellos empleados que trabajen en oficinas de América.
+
+SELECT first_name, last_name
+FROM employees e
+	JOIN departments d USING (department_id)
+	JOIN locations l USING (location_id)
+	JOIN countries c USING (country_id)
+	JOIN regions r USING (region_id)
+WHERE region_name = 'Americas';
+
+--5: Seleccionar el nombre y apellidos de los hijos, así como el nombre y 
+--apellidos de sus padres, para aquellos empleados que trabajen en oficinas 
+--de América. Ordena la salida por orden alfabético del país
+
+SELECT dd.first_name, dd.last_name, e.first_name, e.last_name
+FROM dependents dd
+	JOIN employees e USING (employee_id)
+	JOIN departments d USING (department_id)
+	JOIN locations l USING (location_id)
+	JOIN countries c USING (country_id)
+	JOIN regions r USING (region_id)
+WHERE region_name = 'Americas'
+ORDER BY country_name;
+
+--6: Diseña una consulta (incluyendo su solución) para la base de datos HR que 
+--contenga los siguientes elementos.
+--La salida del select no será SELECT *
+--Debe realizar el JOIN de al menos 3 tablas.
+--Uno de los JOINs debe, obligatoriamente, ser un JOIN ON
+--Al menos, tendrá dos condiciones en el WHERE (conectadas con AND u OR)
+--Debe ordenar la salida por algún criterio.
+
+SELECT e.first_name ,  e.last_name  
+FROM employees e
+	JOIN employees j ON (e.manager_id = j.employee_id)
+	JOIN departments d ON (e.department_id = d.department_id)
+	JOIN locations l USING (location_id)
+	JOIN countries c USING (country_id)
+	JOIN regions r USING (region_id)
+WHERE j.first_name = 'Alexander'
+	AND j.last_name = 'Hunold'
+	AND r.region_name = 'Americas';
+
