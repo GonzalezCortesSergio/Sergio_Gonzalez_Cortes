9981aa45ce8093bff7b009112e8e96ac8efc2432
Sergio González Cortés <gonzalez.coser23@triana.salesianos.edu>
2023-12-04 20:13:07 +0100

    Ejercicio 1 BDD terminado

--- Base de Datos/UD5/Boletín de ejercicios 1/Ejercicio 1.sql
+++ Base de Datos/UD5/Boletín de ejercicios 1/Ejercicio 1.sql
@@ -36,5 +36,43 @@ oficina, mostrando el STREET_ADDRESS y dicho número; hay que
 ordenar la salida de mayor a menor.
 */
 
-SELECT
+SELECT street_address, department_name, COUNT (e.*) AS "numero_empleados"
 FROM employees e
+	JOIN departments d USING (department_id)
+	JOIN locations l USING (location_id)
+GROUP BY street_address, department_name
+ORDER BY numero_empleados DESC;
+
+
+/*
+4. Modificar la consulta anterior para que muestre las 
+localizaciones que no tienen ningún empleado.
+*/
+
+SELECT street_address, department_name, COUNT (e.*) AS "numero_empleados"
+FROM employees e
+	JOIN departments d USING (department_id)
+	JOIN locations l USING (location_id)
+GROUP BY street_address, department_name
+ORDER BY numero_empleados DESC;
+
+/*
+5. Seleccionar el salario que es cobrado a la vez por 
+más personas. Mostrar dicho salario y el número de personas.
+*/
+
+SELECT salary, COUNT (e.*) AS "numero_empleados"
+FROM employees e
+GROUP BY salary;
+
+
+/*
+6. Seleccionar el número de empleados que empezaron a 
+trabajar cada año, ordenando la salida por el año.
+*/
+
+SELECT EXTRACT (year from hire_date) AS "año_contratado", COUNT (e.*)
+FROM employees e
+GROUP BY año_contratado
+ORDER BY año_contratado;
+
