9719ad04466f1fb7db43f2f43a0bec4f8d418a8b
Sergio González Cortés <gonzalez.coser24@triana.salesianos.edu>
2024-03-11 10:01:48 +0100

    Tema 8 empezado y boletín también

---
+++ Lenguaje de marcas/GonzálezCortésSergio_Boletin8/ejercicio4/script.js
@@ -0,0 +1,31 @@
+let valores = [true, 5, false, "hola", "adios", 2];
+
+let demostracion;
+/*if (valores[3].length > valores[4].length) {
+
+    demostracion = valores[3];
+}
+
+if (valores[4].length > valores[3].length) {
+
+    demostracion = valores[4];
+}*/
+
+if (valores [3].length > valores[4].length) {
+
+    alert(valores[3]);
+}
+
+if (valores[4].length > valores[3].length) {
+
+    alert(valores[4]);
+}
+
+alert(valores[0]);
+alert(valores[2]);
+
+//alert(demostracion);
+alert(valores[1] + valores[5]);
+alert(valores[1] - valores[5]);
+alert(valores[1] * valores[5]);
+alert(valores[1] / valores[5]);
