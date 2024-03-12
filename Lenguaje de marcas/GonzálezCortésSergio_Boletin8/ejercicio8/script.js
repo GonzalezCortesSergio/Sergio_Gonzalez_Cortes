b9993f7aade1b659c7a7d0d6fbd3eb4fd8e8bace
Sergio González Cortés <gonzalez.coser24@triana.salesianos.edu>
2024-03-12 09:11:27 +0100

    Arreglitos en fallos que habían en los condicionales

--- Lenguaje de marcas/GonzálezCortésSergio_Boletin8/ejercicio8/script.js
+++ Lenguaje de marcas/GonzálezCortésSergio_Boletin8/ejercicio8/script.js
@@ -6,13 +6,13 @@ function parOImpar (nEntero) {
     return nEntero % 2;
 }
 
-if (parOImpar == 0) {
+if (parOImpar(nEntero) == 0) {
 
     alert ("El número " + nEntero + " es par, ya que el resto de su división entre 2 es igual a " + parOImpar(nEntero));
 }
 
-else {
+if (parOImpar(nEntero) != 0) {
 
-    alert ("El número " + nEntero + "es impar, ya que el resto de su división entre 2 es igual a "+ parOImpar(nEntero));
+    alert ("El número " + nEntero + " es impar, ya que el resto de su división entre 2 es igual a "+ parOImpar(nEntero));
 }
 
