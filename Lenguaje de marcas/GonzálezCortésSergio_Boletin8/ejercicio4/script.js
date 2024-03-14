446bf3ae0c8cc6955b3ba0edcccde5eb91fae62e
Sergio González Cortés <gonzalez.coser24@triana.salesianos.edu>
2024-03-14 09:49:21 +0100

    *Cositas*

--- Lenguaje de marcas/GonzálezCortésSergio_Boletin8/ejercicio4/script.js
+++ Lenguaje de marcas/GonzálezCortésSergio_Boletin8/ejercicio4/script.js
@@ -1,31 +1,15 @@
 let valores = [true, 5, false, "hola", "adios", 2];
 
-let demostracion;
-/*if (valores[3].length > valores[4].length) {
 
-    demostracion = valores[3];
-}
+let mayor = (valores[3] > valores[4])?valores[3]:valores[4];
 
-if (valores[4].length > valores[3].length) {
+alert ("El mayor es " + mayor);
 
-    demostracion = valores[4];
-}*/
+alert(valores[0] || valores[2]);
+alert(valores[0] && valores[2]);
 
-if (valores [3].length > valores[4].length) {
-
-    alert(valores[3]);
-}
-
-if (valores[4].length > valores[3].length) {
-
-    alert(valores[4]);
-}
-
-alert(valores[0]);
-alert(valores[2]);
-
-//alert(demostracion);
 alert(valores[1] + valores[5]);
 alert(valores[1] - valores[5]);
 alert(valores[1] * valores[5]);
 alert(valores[1] / valores[5]);
+alert (valores[1] % valores[5]);
