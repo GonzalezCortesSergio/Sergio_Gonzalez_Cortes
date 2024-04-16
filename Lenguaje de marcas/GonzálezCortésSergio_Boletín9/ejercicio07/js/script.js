39fb933dcfcc5d7e86a8870c65ea82501e216e53
Sergio González Cortés <gonzalez.coser24@triana.salesianos.edu>
2024-04-16 14:06:57 +0200

    Ejercicio 7 del boletín 9 de Lenguaje de Marcas casi terminado

---
+++ Lenguaje de marcas/GonzálezCortésSergio_Boletín9/ejercicio07/js/script.js
@@ -0,0 +1,32 @@
+let formulario = document.forms.formulario;
+
+formulario.addEventListener("submit", comprobarCositas);
+
+function comprobarDni() {
+
+    let dni = formulario.elements.dni.value;
+    let patron = /^(\d{8})([A-Z])$/;
+
+    if (!patron.test(dni)) {
+
+        alert("El DNI debe contener 8 números y una letra")
+    }
+
+}
+
+function comprobarContrasenia() {
+
+    let contrasenia = formulario.elements.contrasenia.value;
+    let repetirContrasenia = formulario.elements.repetirContrasenia.value;
+
+    if (contrasenia !== repetirContrasenia) {
+
+        alert("Las dos contraseñas no coinciden");
+    }
+}
+
+function comprobarCositas() {
+
+    comprobarDni();
+    comprobarContrasenia();
+}
